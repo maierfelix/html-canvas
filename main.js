8562915dc89f603c78fccb771e2ada351f992efa
Felix Maier <xilefmai@gmail.com>
2017-02-12 17:51:45 +0100

    Update
    - Added hdpi rendering
    - Removed watcher.min.js
    - Faster rendering

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let el = document.getElementById("htmlgl");
 
-  let stage = new Stage({
+  let stage = new HTMLRenderer({
     alpha: 0.25, // base alpha
     element: el // element to render
   });
@@ -17,10 +17,15 @@ document.addEventListener("DOMContentLoaded", () => {
     stage.render();
   };
 
-  window.addEventListener("resize", function() {
+  window.addEventListener("resize", () => {
     let width = window.innerWidth;
     let height = window.innerHeight;
     stage.resize(width, height);
+  });
+
+  window.addEventListener("mousewheel", (e) => {
+    let x = e.deltaY > 0 ? -0.1 : 0.1;
+    stage.scale += x;
   });
 
 });
